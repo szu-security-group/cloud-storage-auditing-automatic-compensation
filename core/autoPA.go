2297a9613c125b2c9eedecfebb8ed7c110a458ca
fchen <51083793+fchen-group@users.noreply.github.com>
2024-01-04 10:42:50 +0800

    Update autoPA.go

--- core/autoPA.go
+++ core/autoPA.go
@@ -1,3 +1,12 @@
+/*
+
+The smart contract was deployed on the latest version of Ethereum.
+We used solidity (version 0.60) to write the smart contract.
+The protocol is implemented using Golang 1.17 (blockchain offline) and solidity (blockchain online).
+The modulus $N$ is 1024 bits and the size of data block is 4KB. The experimental data ranges from 100MB to 1GB.
+
+*/
+
 package core
 
 import (
