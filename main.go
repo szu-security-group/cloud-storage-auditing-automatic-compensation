1288137bc3018fa3716b9d11f4a75897e64e3d7e
fchen <51083793+fchen-group@users.noreply.github.com>
2025-10-27 10:21:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 var SecurityLevel = 1024
 var DataSize = "16MB"
 var GoNumber = 16
-var ChallengeNumber = 460
+var ChallengeNumber = 10
 var windowSize = 20
 
 func main() {
@@ -31,47 +31,36 @@ func main() {
 	// divide data block
 	fileSet := data.DivideInto4kb(f)
 
+	timeCollect.Start("BuildMHT")
+	core.BuildMHT(fileSet)
+	timeCollect.End()
+
 	sk, pk := core.KeySetup(SecurityLevel)
 
 	timeCollect.Start("TagGenerate")
 	core.TagGenerate(&fileSet, sk, pk, GoNumber)
 	timeCollect.End()
 
-	Type := 0 // 0 represent HVT based protocol，and 1 represent BAT based protocol
-	if Type == 1 {
-		s, gs := core.GetGs(pk.N, sk.G)
-		fmt.Println("GsSet SUCCESS")
+	timeCollect.Start("AggregateTag")
+	aggTs := core.AggregateTag(&fileSet, pk, windowSize, windowSize)
+	timeCollect.End()
 
-		chalSuite := core.SetChallengeWithHVT(&fileSet, ChallengeNumber, s, pk.N)
-		fmt.Println("ChallengeTagSet SUCCESS")
+	timeCollect.Start("Challenge")
+	s, gs := core.GetGs(pk.N, sk.G)
 
-		proof := core.ProofGen(&fileSet, chalSuite.Index, gs, pk.N)
-		fmt.Println("ProofGen Success SUCCESS")
+	chalSuitea := core.SetChallengeTagWithAggregation(aggTs, ChallengeNumber, s, pk.N)
+	timeCollect.End()
+	fmt.Println("Aggregate ChallengeTags SUCCESS")
 
-		result := core.Verify(chalSuite, proof, pk.E, pk.N)
-		fmt.Println("Verify Success SUCCESS")
-		fmt.Println(result)
+	timeCollect.Start("GenerateProof")
+	proofa := core.ProofGenWithAggregation(fileSet, chalSuitea.Index, gs, pk.N)
+	timeCollect.End()
+	fmt.Println("Aggregate ProofGen SUCCESS")
 
-	} else {
-		timeCollect.Start("AggregateTag")
-		aggTs := core.AggregateTag(&fileSet, pk, windowSize, windowSize)
-		timeCollect.End()
+	// implement it on blockchain
+	_ = core.VerifyWithAggregation(chalSuitea, proofa, pk.E, pk.N)
+	fmt.Printf("Aggregate Verify SUCCESS\n")
 
-		timeCollect.Start("Challenge")
-		s, gs := core.GetGs(pk.N, sk.G)
-		chalSuitea := core.SetChallengeTagWithAggregation(aggTs, 460, s, pk.N)
-		timeCollect.End()
-		fmt.Println("Aggregate ChallengeTags SUCCESS")
-
-		timeCollect.Start("GenerateProof")
-		proofa := core.ProofGenWithAggregation(fileSet, chalSuitea.Index, gs, pk.N)
-		timeCollect.End()
-		fmt.Println("Aggregate ProofGen SUCCESS")
-
-		// implement it on blockchain
-		_ = core.VerifyWithAggregation(chalSuitea, proofa, pk.E, pk.N)
-		fmt.Printf("Aggregate Verify SUCCESS")
-	}
 	timeCollect.Output()
 }
 
